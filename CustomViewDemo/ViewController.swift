e6b318d95726996c80134d3bc12aa12c4b121a7f
Mustofa Mahmud <mustofa.mahmud@bjitgroup.com>
2022-12-07 12:27:54 +0600

    Initial Commit

---
+++ CustomViewDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CustomViewDemo
+//
+//  Created by BJIT on 7/12/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
